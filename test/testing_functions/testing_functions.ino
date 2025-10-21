11f2ec57e371890bf4cd5067f97c8f48223e646b
kutman malikov <kutman.engineer@gmail.com>
2025-10-22 00:43:55 +0300

    Update testing_functions.ino

--- test/testing_functions/testing_functions.ino
+++ test/testing_functions/testing_functions.ino
@@ -1,10 +1,17 @@
+#include <Arduino.h>
+
+#define BUTTON_PIN 34 // GIOP21 pin connected to button
 void setup() {
-  // put your setup code here, to run once:
+  // initialize serial communication at 9600 bits per second:
   Serial.begin(115200);
+  // initialize the pushbutton pin as an pull-up input
+  // the pull-up input pin will be HIGH when the switch is open and LOW when the switch is closed.
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println("Hello World!");
-  delay(500);
+  // read the state of the switch/button:
+  int buttonState = digitalRead(BUTTON_PIN);
+  // print out the button's state
+  Serial.println(buttonState);
 }
